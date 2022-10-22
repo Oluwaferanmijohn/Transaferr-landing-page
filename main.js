e63320514153f3280522399e44ecab3ded71a5fe
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-10-22 21:45:58 +0100

    phenry

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ let email = document.querySelector(".userEmail")
 let getBtn = document.querySelector("#getUserEmail")
 let errorMessage = document.getElementById('errorMessage')
 let errorMessageOne = document.getElementById('errorMessageOne')
+let errorMessageTwo = document.getElementById('errorMessageTwo')
 const emailVal = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 getBtn.addEventListener("click", async () => {
     if (!emailVal.test(email.value)) {
-
-        errorMessage.innerText += `enter a vaild email address`
-        errorMessage.style.color = 'red'
+        errorMessageTwo.style.display = 'block';
+        errorMessageTwo.innerText += `enter a vaild email address`
+       
         window.location.reload()
     } else {
+        errorMessage.style.display = 'block'
         errorMessage.innerText += `processing email address`
         let currEmail = email.value
 
@@ -24,12 +26,13 @@ getBtn.addEventListener("click", async () => {
                 const data = {
                     email: currEmail
                 }
-                let randomUserId = 'VIC' + Math.floor(Math.random() * 23748495 + 20000)
+                let randomUserId = 'PHE' + Math.floor(Math.random() * 23748495 + 20000)
                 const docRef = doc(db, 'users', randomUserId)
                 let res = await setDoc(docRef, data)
                 // console.log(res)
                 errorMessage.style.display = 'none'
-                errorMessageOne.innerText += `wow! registered.`
+                errorMessageOne.style.display = 'block'
+                errorMessageOne.innerText += `Email Registered Successfully!`
                 // alert("success")
                 window.location.reload()
 
