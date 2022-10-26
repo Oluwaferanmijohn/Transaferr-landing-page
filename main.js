5d7169d9d08946ebd9f491fc9de44d98b521b97b
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-10-26 22:53:22 +0100

    phenry

--- main.js
+++ main.js
@@ -8,18 +8,30 @@ let getBtn = document.querySelector("#getUserEmail")
 let errorMessage = document.getElementById('errorMessage')
 let errorMessageOne = document.getElementById('errorMessageOne')
 let errorMessageTwo = document.getElementById('errorMessageTwo')
+let xOne = document.getElementById('xOne')
+let xTwo = document.getElementById('xTwo')
+let xThree = document.getElementById('xThree')
 const emailVal = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+xOne.addEventListener("click", () =>{
+    errorMessageTwo.style.display = 'none';
+})
+xTwo.addEventListener("click", () =>{
+    errorMessageOne.style.display = 'none'
+})
+xThree.addEventListener("click", () =>{
+    errorMessage.style.display = 'none'
+})
 getBtn.addEventListener("click", async () => {
     if (!emailVal.test(email.value)) {
-        errorMessageTwo.style.display = 'block';
+        errorMessageTwo.style.display = 'flex';
         // errorMessageTwo.innerText += `enter a vaild email address`
        
         // window.location.reload()
     } else {
-        errorMessage.style.display = 'block'
+        errorMessage.style.display = 'flex'
         // errorMessage.innerText += `processing email address`
         errorMessageTwo.style.display = 'none';
+        errorMessageOne.style.display = 'none'
         let currEmail = email.value
 
         if (currEmail.length > 0) {
@@ -33,7 +45,7 @@ getBtn.addEventListener("click", async () => {
                 // console.log(res)
                 errorMessage.style.display = 'none'
                 errorMessageTwo.style.display = 'none'
-                errorMessageOne.style.display = 'block'
+                errorMessageOne.style.display = 'flex'
                 // errorMessageOne.innerText += `Email Registered Successfully!`
                 // alert("success")
                 // window.location.reload()
