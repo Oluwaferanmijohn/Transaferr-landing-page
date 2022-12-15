a597228adec4b2f5f03a2cc44c647ec5b2b1f29b
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-12-15 20:36:04 +0100

    new changes

--- team/team.js
+++ team/team.js
@@ -1,6 +1,8 @@
 let aside = document.getElementById('asidebar')
  let getStarted = document.getElementById('getStarted');
  let getStarted2 = document.getElementById('getStarted2');
+ let getStarted3 = document.getElementById('getStarted3');
+ let getStarted4 = document.getElementById('getStarted4');
 
  getStarted.addEventListener("click", () =>{
      aside.style.display = 'grid';
@@ -13,5 +15,20 @@ let aside = document.getElementById('asidebar')
      aside.style.display = 'none';
      getStarted.style.display = 'flex';
      getStarted2.style.display = 'none';
+ })
+
+ getStarted3.addEventListener("click", () =>{
+     aside.style.display = 'grid';
+    //  getStarted.style.display = 'none';
+     getStarted3.style.display = 'none';
+     getStarted4.style.display = 'flex';
+
+
+ })
+ getStarted4.addEventListener("click", () =>{
+     aside.style.display = 'none';
+    //  getStarted.style.display = 'flex';
+     getStarted3.style.display = 'flex';
+     getStarted4.style.display = 'none';
  })
 // alert('working')
